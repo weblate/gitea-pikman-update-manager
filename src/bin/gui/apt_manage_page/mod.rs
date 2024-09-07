d0837b5b8b689cfda60796baa3d61cd05e99a139
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-09-07 13:44:34 +0300

    add labels

--- src/bin/gui/apt_manage_page/mod.rs
+++ src/bin/gui/apt_manage_page/mod.rs
@@ -41,11 +41,38 @@ pub fn apt_manage_page(
         .orientation(Orientation::Vertical)
         .build();
 
+    let system_mirror_label0 = gtk::Label::builder()
+        .label(t!("system_mirror_label0_label"))
+        .halign(gtk::Align::Start)
+        .valign(gtk::Align::Start)
+        .hexpand(true)
+        .margin_top(15)
+        .margin_start(15)
+        .margin_end(15)
+        .build();
+    system_mirror_label0.add_css_class("heading");
+
+    let system_mirror_label1 = gtk::Label::builder()
+        .label(t!("system_mirror_label1_label"))
+        .halign(gtk::Align::Start)
+        .valign(gtk::Align::Start)
+        .hexpand(true)
+        .margin_start(15)
+        .margin_end(15)
+        .build();
+
     let system_mirror_entry = gtk::Entry::builder()
         .placeholder_text(system_source.repolib_default_mirror.as_deref().unwrap())
         .text(system_source.uris.as_deref().unwrap())
+        .valign(gtk::Align::Start)
+        .margin_top(5)
+        .margin_bottom(5)
+        .margin_start(15)
+        .margin_end(15)
         .build();
 
+    main_box.append(&system_mirror_label0);
+    main_box.append(&system_mirror_label1);
     main_box.append(&system_mirror_entry);
 
     main_box
