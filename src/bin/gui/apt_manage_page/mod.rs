4e28e295ae8d74f308719e21d7b36f1361ee67e0
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-09-18 18:15:19 +0300

    basic col item integration

--- src/bin/gui/apt_manage_page/mod.rs
+++ src/bin/gui/apt_manage_page/mod.rs
@@ -4,7 +4,9 @@ use adw::prelude::*;
 use apt_deb822_tools::Deb822Repository;
 use gtk::glib::*;
 use gtk::*;
+use std::cell::Ref;
 use pika_unixsocket_tools::pika_unixsocket_tools::*;
+use property::PropertyGet;
 use rust_apt::cache::*;
 use rust_apt::new_cache;
 use rust_apt::records::RecordField;
@@ -13,7 +15,6 @@ use std::process::Command;
 use std::rc::Rc;
 use std::thread;
 use tokio::runtime::Runtime;
-
 
 pub fn apt_manage_page(
     window: adw::ApplicationWindow,
@@ -97,6 +98,91 @@ pub fn apt_manage_page(
         .margin_end(15)
         .build();
 
+    let unofficial_sources_list_store = gio::ListStore::new::<BoxedAnyObject>();
+
+    struct Row2 {
+        col1: String,
+        col2: String,
+    }
+
+    (0..10000).for_each(|i| {
+        unofficial_sources_list_store.append(&BoxedAnyObject::new(Row2 {
+            col1: format!("col1 {i}"),
+            col2: format!("col2 {i}"),
+        }))
+    });
+
+    let unofficial_sources_selection_model = SingleSelection::new(Some(unofficial_sources_list_store));
+
+    /*let unofficial_sources_item_factory = SignalListItemFactory::new();
+
+    unofficial_sources_item_factory.connect_setup(|_item_factory, list_item| {
+        let label = gtk::Label::new(Some("DDD"));
+
+        let list_item = list_item.downcast_ref::<gtk::ListItem>().unwrap();
+
+        list_item.set_child(Some(&label));
+    });
+
+    unofficial_sources_item_factory.connect_bind(|_item_factory, list_item| {
+        let list_item = list_item.downcast_ref::<gtk::ListItem>().unwrap();
+
+        let list_item_item = list_item.item().unwrap().to_value().get::<String>().unwrap();
+        let list_item_label = list_item.child().unwrap().downcast::<gtk::Label>().unwrap();
+
+        list_item_label.set_label(&list_item_item);
+    });
+    */
+
+    let unofficial_sources_columnview = ColumnView::builder()
+        .margin_bottom(3)
+        .margin_top(3)
+        .margin_end(3)
+        .margin_start(3)
+        .vexpand(true)
+        .model(&unofficial_sources_selection_model)
+        .build();
+
+        let col1factory = gtk::SignalListItemFactory::new();
+    let col2factory = gtk::SignalListItemFactory::new();
+    col1factory.connect_setup(move |_factory, item| {
+        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+        let row = Label::default();
+        item.set_child(Some(&row));
+    });
+
+    col1factory.connect_bind(move |_factory, item| {
+        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+        let child = item.child().and_downcast::<Label>().unwrap();
+        let entry = item.item().and_downcast::<BoxedAnyObject>().unwrap();
+        let r: Ref<Row2> = entry.borrow();
+        child.set_label(&r.col1.to_string());
+    });
+    col2factory.connect_setup(move |_factory, item| {
+        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+        let row = Label::default();
+        item.set_child(Some(&row));
+    });
+
+    col2factory.connect_bind(move |_factory, item| {
+        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+        let child = item.child().and_downcast::<Label>().unwrap();
+        let entry = item.item().and_downcast::<BoxedAnyObject>().unwrap();
+        let r: Ref<Row2> = entry.borrow();
+        child.set_label(&r.col2.to_string());
+    });
+    let col1: ColumnViewColumn = gtk::ColumnViewColumn::new(Some("Column 1"), Some(col1factory));
+    let col2 = gtk::ColumnViewColumn::new(Some("Column 2"), Some(col2factory));
+    col1.set_expand(true);
+    unofficial_sources_selection_model.connect_selected_item_notify(|selection| {
+        let selection = selection.selected_item().unwrap();
+        let entry  = selection.downcast_ref::<BoxedAnyObject>().unwrap();
+        let r: Ref<Row2> = entry.borrow();
+        println!("{}", r.col2.to_string())
+    });
+    unofficial_sources_columnview.append_column(&col1);
+    unofficial_sources_columnview.append_column(&col2);
+
     let unofficial_sources_boxedlist = ListBox::builder()
         .selection_mode(SelectionMode::None)
         .margin_bottom(3)
@@ -113,7 +199,7 @@ pub fn apt_manage_page(
         .margin_top(15)
         .margin_end(15)
         .margin_start(15)
-        .child(&unofficial_sources_boxedlist)
+        .child(&unofficial_sources_columnview)
         .height_request(390)
         .build();
     unofficial_sources_viewport.add_css_class("round-all-scroll");
