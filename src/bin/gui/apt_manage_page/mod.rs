5e00e676e9050c58d5a661a20827120a05e62afc
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-09-19 21:13:08 +0300

    add apt repo tools integration

--- src/bin/gui/apt_manage_page/mod.rs
+++ src/bin/gui/apt_manage_page/mod.rs
@@ -5,6 +5,7 @@ use apt_deb822_tools::Deb822Repository;
 use gtk::glib::*;
 use gtk::*;
 use std::cell::Ref;
+use std::ops::Deref;
 use pika_unixsocket_tools::pika_unixsocket_tools::*;
 use property::PropertyGet;
 use rust_apt::cache::*;
@@ -23,18 +24,27 @@ pub fn apt_manage_page(
 
     let deb822_sources = Deb822Repository::get_deb822_sources().unwrap();
 
+    let mut unofficial_deb822_sources = deb822_sources.clone();
+
     let system_source = deb822_sources.iter().filter(|x| {
         match &x.repolib_id {
             Some(t) => {
-                if t == "system" {
-                    true
-                } else {
-                    false
-                }
+                t == "system"
             }
             None => false
         }
     }).next().unwrap();
+    
+    unofficial_deb822_sources.retain(|x| {
+        match &x.repolib_id {
+            Some(t) => {
+                !(t == "system")
+            }
+            None => true
+        }
+    });
+
+    let legacy_apt_repos = apt_legacy_tools::LegacyAptSource::get_legacy_sources();
 
     let main_box = Box::builder()
         .hexpand(true)
@@ -100,17 +110,23 @@ pub fn apt_manage_page(
 
     let unofficial_sources_list_store = gio::ListStore::new::<BoxedAnyObject>();
 
-    struct Row2 {
-        col1: String,
-        col2: String,
+    enum AptSourceConfig {
+        Legacy(apt_legacy_tools::LegacyAptSource),
+        DEB822(apt_deb822_tools::Deb822Repository)
     }
 
-    (0..10000).for_each(|i| {
-        unofficial_sources_list_store.append(&BoxedAnyObject::new(Row2 {
-            col1: format!("col1 {i}"),
-            col2: format!("col2 {i}"),
-        }))
-    });
+    for deb822_source in unofficial_deb822_sources {
+        unofficial_sources_list_store.append(&BoxedAnyObject::new(AptSourceConfig::DEB822(deb822_source)));
+    };
+
+    match legacy_apt_repos {
+        Ok(vec) => {
+            for legacy_repo in vec {
+                unofficial_sources_list_store.append(&BoxedAnyObject::new(AptSourceConfig::Legacy(legacy_repo)));
+            };
+        }
+        Err(_) => {}
+    }
 
     let unofficial_sources_selection_model = SingleSelection::new(Some(unofficial_sources_list_store));
 
@@ -143,45 +159,117 @@ pub fn apt_manage_page(
         .model(&unofficial_sources_selection_model)
         .build();
 
-        let col1factory = gtk::SignalListItemFactory::new();
-    let col2factory = gtk::SignalListItemFactory::new();
-    col1factory.connect_setup(move |_factory, item| {
+    //
+
+    let unofficial_sources_columnview_factory0 = gtk::SignalListItemFactory::new();
+    
+    unofficial_sources_columnview_factory0.connect_setup(move |_factory, item| {
         let item = item.downcast_ref::<gtk::ListItem>().unwrap();
-        let row = Label::default();
+        let row = Label::builder()
+            .halign(Align::Start)
+            .build();
         item.set_child(Some(&row));
     });
 
-    col1factory.connect_bind(move |_factory, item| {
-        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+    unofficial_sources_columnview_factory0.connect_bind(move |_factory, item| {
+        let item: &ListItem = item.downcast_ref::<gtk::ListItem>().unwrap();
         let child = item.child().and_downcast::<Label>().unwrap();
-        let entry = item.item().and_downcast::<BoxedAnyObject>().unwrap();
-        let r: Ref<Row2> = entry.borrow();
-        child.set_label(&r.col1.to_string());
+        let entry: BoxedAnyObject = item.item().and_downcast::<BoxedAnyObject>().unwrap();
+        let entry_borrow = entry.borrow::<AptSourceConfig>();
+        let repo_name = match entry_borrow.deref() {
+            AptSourceConfig::DEB822(src) => {
+                match &src.repolib_name {
+                    Some(name) => name,
+                    None => match(&src.uris, &src.suites, &src.components) {
+                        (Some(uris),Some(suites),Some(components)) => {
+                            &format!("{} {} {}", uris, suites, components)
+                        }
+                        (_,_,_) => {
+                            &t!("apt_source_parse_error").to_string()
+                        }
+                    }
+                    
+                    
+                }
+            }
+            AptSourceConfig::Legacy(src) => {
+                &format!("{} {} {} {}",
+                    if src.is_source {
+                        "(Legacy Src)"
+                    } else {
+                        "(Legacy)"
+                    },
+                    &src.url,
+                    &src.suite,
+                    &src.components
+                )
+            }
+        };
+        child.set_label(&repo_name);
     });
-    col2factory.connect_setup(move |_factory, item| {
+    
+    let unofficial_sources_columnview_col0 = gtk::ColumnViewColumn::builder()
+        .title(t!("unofficial_sources_columnview_col0_title"))
+        .factory(&unofficial_sources_columnview_factory0)
+        .expand(true)
+        .build();
+
+    //
+
+    let unofficial_sources_columnview_factory1 = gtk::SignalListItemFactory::new();
+    
+    unofficial_sources_columnview_factory1.connect_setup(move |_factory, item| {
         let item = item.downcast_ref::<gtk::ListItem>().unwrap();
-        let row = Label::default();
+        let row = Label::builder()
+            .halign(Align::Start)
+            .build();
         item.set_child(Some(&row));
     });
 
-    col2factory.connect_bind(move |_factory, item| {
-        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+    unofficial_sources_columnview_factory1.connect_bind(move |_factory, item| {
+        let item: &ListItem = item.downcast_ref::<gtk::ListItem>().unwrap();
         let child = item.child().and_downcast::<Label>().unwrap();
-        let entry = item.item().and_downcast::<BoxedAnyObject>().unwrap();
-        let r: Ref<Row2> = entry.borrow();
-        child.set_label(&r.col2.to_string());
+        let entry: BoxedAnyObject = item.item().and_downcast::<BoxedAnyObject>().unwrap();
+        let entry_borrow = entry.borrow::<AptSourceConfig>();
+        let repo_enabled = match entry_borrow.deref() {
+            AptSourceConfig::DEB822(src) => {
+                match &src.enabled {
+                    Some(t) => match t.to_lowercase().as_str() {
+                        "yes" => true,
+                        "true" => true,
+                        "no" => false,
+                        "false" => false,
+                        _ => true,
+                    }
+                    None => true,
+                }
+            }
+            AptSourceConfig::Legacy(src) => {
+                src.enabled
+            }
+        };
+        if repo_enabled {
+            child.set_label(&t!("apt_repo_enabled"));
+        } else {
+            child.set_label(&t!("apt_repo_disabled"));
+        }
     });
-    let col1: ColumnViewColumn = gtk::ColumnViewColumn::new(Some("Column 1"), Some(col1factory));
-    let col2 = gtk::ColumnViewColumn::new(Some("Column 2"), Some(col2factory));
-    col1.set_expand(true);
+    
+    let unofficial_sources_columnview_col1 = gtk::ColumnViewColumn::builder()
+        .title(t!("unofficial_sources_columnview_col1_title"))
+        .factory(&unofficial_sources_columnview_factory1)
+        .build();
+
+    //
+
     unofficial_sources_selection_model.connect_selected_item_notify(|selection| {
         let selection = selection.selected_item().unwrap();
         let entry  = selection.downcast_ref::<BoxedAnyObject>().unwrap();
-        let r: Ref<Row2> = entry.borrow();
-        println!("{}", r.col2.to_string())
+        //let r: Ref<AptSourceConfig> = entry.borrow();
+        //println!("{}", r.col2.to_string())
     });
-    unofficial_sources_columnview.append_column(&col1);
-    unofficial_sources_columnview.append_column(&col2);
+    unofficial_sources_columnview.append_column(&unofficial_sources_columnview_col0);
+    unofficial_sources_columnview.append_column(&unofficial_sources_columnview_col1);
 
     let unofficial_sources_boxedlist = ListBox::builder()
         .selection_mode(SelectionMode::None)
