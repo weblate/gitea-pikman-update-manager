5ba4222c1aee50e446742e106166c671313387d6
ferreo <ferreo@noreply.pika>
2024-11-15 17:38:33 +0100

    Update pika-build-config/amd64-v3.sh

--- pika-build-config/amd64-v3.sh
+++ pika-build-config/amd64-v3.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 export PIKA_BUILD_ARCH="amd64-v3"
 export DEBIAN_FRONTEND="noninteractive"
-export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto=auto"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto=auto"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto=auto"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto=auto"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto=auto"
 export DEB_BUILD_OPTIONS="nocheck notest terse"
 export DPKG_GENSYMBOLS_CHECK_LEVEL=0
